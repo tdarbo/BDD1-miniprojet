347c23e89d3d0d50b4705e4b5b1e6d0ba8e50ba4
Helytre <matthieu.tironneau@efrei.net>
2026-03-15 12:35:45 +0100

    Update 4_scenario_1.sql

--- 4_scenario_1.sql
+++ 4_scenario_1.sql
@@ -1,9 +1,13 @@
+-- liste des passagers
+
 select Nom,Prénom,nom_pays
 from (
 select Nom,Prénom,Code_pays from Voyageur join Vol_voyageur using (UUID_voyageur) where UUID_vol='vol-0001-0001-0001-0001-000000000001'
 union all 
 select Nom,Prénom,Code_pays from Employer join Vol_employer using (UUID_employer) where UUID_vol='vol-0001-0001-0001-0001-000000000001'
 ) as a_sauver join pays using (code_pays) order by nom_pays,nom,prénom asc;
+
+-- creation et changement du statut pour les avions
 
 alter table avion
 add column statut_avion varchar(50)
@@ -17,6 +21,8 @@ alter table avion
 add constraint ck_statut_avion check (statut_avion in ('actif','en maintenance'));
 
 select ID_avion, statut_avion from avion;
+
+-- creation et changement du statut pour les employers
 
 alter table employer
 add column statut_employer varchar(50)
